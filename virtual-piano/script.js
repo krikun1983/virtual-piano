0e79f2a846f11194ad12f84700330a2e3209d0e4
Stanislav Akhmadeev <akhmadeevst@gmail.com>
2021-03-20 11:41:30 +0400

    feat: end press keyboard

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -10,21 +10,74 @@ const btnLetters = document.querySelector('.btn-letters');
 //меняю класс у кнопок
 btnNotes.onclick = function () {
     if (btnLetters.classList.contains('btn-active')) {
-        btnLetters.classList.toggle('btn-active');
-        btnNotes.classList.toggle('btn-active');
+        btnNotes.classList.add('btn-active');
+        btnLetters.classList.remove('btn-active');
     }
     pianoКeys.forEach(elem => {
-        elem.classList.remove('piano-key-letter');
+        elem.classList.remove('piano-key-letter'); //чтобы не нажималось дважды на кнопку
     })
 }
 
 btnLetters.onclick = function () {
     if (btnNotes.classList.contains('btn-active')) {
-        btnLetters.classList.toggle('btn-active');
-        btnNotes.classList.toggle('btn-active');
+        btnLetters.classList.add('btn-active');
+        btnNotes.classList.remove('btn-active');
     }
 
     pianoКeys.forEach(elem => {
-        elem.classList.add('piano-key-letter');
+        elem.classList.add('piano-key-letter'); //чтобы не нажималось дважды на кнопку
     })
 }
+
+//События мыши
+
+piano.addEventListener('click', (event) => {
+    event.target.classList.add('piano-key-active');
+})
+
+//События клавиатуры
+let fired = false;
+
+window.addEventListener('keydown', (event) => {
+    if (!fired) {
+        fired = true;
+        pianoКeys.forEach(elem => {
+            if (event.code === `Key${elem.getAttribute('data-letter')}`) {
+                elem.classList.add('piano-key-active');//клавиши изменяются
+                playAudio(`assets/audio/${elem.getAttribute('data-note')}.mp3`);
+            }
+        })
+    }
+});
+
+//чтобы срабатывала один раз
+window.addEventListener('keyup', (event) => {
+    fired = false;
+    pianoКeys.forEach(elem => {
+        if (event.code === `Key${elem.getAttribute('data-letter')}`) {
+            elem.classList.remove('piano-key-active');
+        }
+    })
+});
+
+
+//создаем функцию аудио
+function playAudio(src) {
+    const audio = new Audio();
+    audio.src = src;
+    audio.currentTime = 0;
+    audio.play();
+}
+
+
+
+
+// console.log(btnNotes);
+
+
+//События мыши
+// piano.addEventListener('click', (event) => playAudio(event));
+
+
+// const url = 'assets/audio/c.mp3';
+// button.addEventListener('click', () => playAudio(url))
